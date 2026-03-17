68812e14303143aff3a454ddf33577869843e63a
Sovereign Titan Genesis <admin@quorumstate.international>
2026-03-17 16:25:25 -0400

    Update QStateToken.sol
    Signed-off-by: Sovereign Titan Genesis <admin@quorumstate.international>

--- src/contracts/QStateToken.sol
+++ src/contracts/QStateToken.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./GlobalSovereignty.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract QStateToken is ERC20 {
-    // Total Suplai: 1.000.000.000 (1 Miliar) $QSTATE
-    uint256 private constant INITIAL_SUPPLY = 1000000000 * 10**18;
-    address public owner;
+contract QStateToken is ERC20, Ownable {
+    // Batas Maksimal: 1.000 Triliun (1.000T)
+    uint256 public constant MAX_SUPPLY = 1000000000000000 * 10**18;
 
-    constructor() ERC20("Quorum State Token", "QSTATE") {
-        owner = msg.sender;
-        _mint(msg.sender, INITIAL_SUPPLY);
+    constructor() ERC20("Quorum State Token", "QSTATE") Ownable(msg.sender) {
+        // Tahap 1: Cetak 1T di awal
+        _mint(msg.sender, 1000000000000 * 10**18);
     }
 
-    // Fungsi untuk cek kedaulatan sebelum transaksi besar (opsional)
-    function transferSovereign(address to, uint256 amount) public returns (bool) {
-        // Logika kedaulatan bisa ditambahkan di sini
-        return transfer(to, amount);
+    // Fungsi untuk mencetak tahap berikutnya (2, 3, dan 4)
+    function mintNextPhase(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Melebihi batas maksimal 1000T!");
+        _mint(to, amount);
     }
 }
